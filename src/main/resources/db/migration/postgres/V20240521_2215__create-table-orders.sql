32341ce5d4d820cc801a4628557f93fdae64b7fc
Jordã Jesus <jordacjesus@gmail.com>
2024-09-25 22:59:25 -0300

    Refact bd_migration to create ERD from pgAdmin

--- src/main/resources/db/migration/postgres/V20240521_2215__create-table-orders.sql
+++ src/main/resources/db/migration/postgres/V20240521_2215__create-table-orders.sql
@@ -2,7 +2,8 @@ CREATE TABLE orders
 (
     order_id BIGSERIAL UNIQUE,
     status character varying(100),
-    customer_id character varying(100),
+    customer_id BIGSERIAL,
     total_order_value double precision DEFAULT 0.0,
-    CONSTRAINT pk_order_id PRIMARY KEY (order_id)
+    CONSTRAINT pk_order_id PRIMARY KEY (order_id),
+    FOREIGN KEY(customer_id) REFERENCES category (id)
 );
